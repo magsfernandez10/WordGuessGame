00348a2da215dbfc2f82512892b98fdecad53ca2
Mags <maggy123@yahoo.com>
2018-09-18 22:58:00 -0500

    updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var words = ["Home","Marge","Bar","Lisa","Maggie","Krust","Moe","Apu","Milhouse","Moe","MrBurns","Ned","Waylon"];
+
+var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var currentword ="";
+var letters = [];
+var currentPuzzle = [];
+var incorrect =[];
+
+var guess ="";
+var wins = 0;
+var guessesRemaining = 10;
+
